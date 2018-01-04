3d4ed412e5e8700312af5fd3c8e87b8d9e94d4f3
jacksonmcgehee <jacksonmcgehee@gmail.com>
2018-01-04 09:31:05 -0500

    Added index route for the workoutController; rendered properly in the browser

--- routes/workoutsController.js
+++ routes/workoutsController.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const Workout = require('../db/models/Workout.js')
 
+router.get('/', (req, res) => {
+    Workout.find({})
+    .then((workout) => {
+        res.render('workouts/index', {
+            workout
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
 
 
 

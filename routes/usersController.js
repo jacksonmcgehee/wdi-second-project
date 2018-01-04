cf1e21b8c7bca8c18f790190eefc285fc42dfc57
jacksonmcgehee <jacksonmcgehee@gmail.com>
2018-01-04 12:07:47 -0500

    Full CRUD for the users

--- routes/usersController.js
+++ routes/usersController.js
@@ -45,6 +45,30 @@ router.get('/:userId', (req, res) => {
     })
 })
 
+router.get('/:userId/edit', (req, res) => {
+  const userId = req.params.userId
+
+  User.findById(userId)
+    .then((user) => {
+      res.render('users/edit', {
+        user,
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
+router.put('/:userId', (req, res) => {
+  const userId = req.params.userId
+  const updatedUserInfo = req.body
+
+  User.findByIdAndUpdate(userId, updatedUserInfo, {new: true})
+    .then(() => {
+      res.redirect(`/users/${userId}`)
+    })
+})
+
 router.get('/:userId/delete', (req, res) => {
   const userId = req.params.userId
   User.findByIdAndRemove(userId)

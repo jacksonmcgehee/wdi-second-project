1cacf644c1b6e96c30a3dd833aba0c206aee65ff
jacksonmcgehee <jacksonmcgehee@gmail.com>
2018-01-04 11:32:07 -0500

    Created the user index route

--- routes/usersController.js
+++ routes/usersController.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../db/models/User.js')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource')
+
+router.get('/', (req, res) => {
+  User.find({})
+    .then((user) => {
+        res.render('users/index', {
+           user
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 })
 
 module.exports = router

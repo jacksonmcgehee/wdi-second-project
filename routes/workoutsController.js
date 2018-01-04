f38d7b1141b802f6945cb52d0ae4a5e7d6ca24b7
jacksonmcgehee <jacksonmcgehee@gmail.com>
2018-01-04 10:57:42 -0500

    Successfully submitted a new workout; successfully deleted an existing workout

--- routes/workoutsController.js
+++ routes/workoutsController.js
@@ -18,6 +18,18 @@ router.get('/new', (req, res) => {
     res.render('workouts/new')
 })
 
+router.post('/', (req, res) => {
+    const newWorkout = req.body
+
+    Workout.create(newWorkout)
+        .then(() => {
+            res.redirect('/workouts')
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
 router.get('/:workoutId', (req, res) => {
     const workoutId = req.params.workoutId
     Workout.findById(workoutId)
@@ -25,6 +37,17 @@ router.get('/:workoutId', (req, res) => {
             res.render('workouts/show', {
                 workout
             })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+router.get('/:workoutId/delete', (req, res) => {
+    const workoutId = req.params.workoutId
+    Workout.findByIdAndRemove(workoutId)
+        .then(() => {
+            res.redirect('/workouts')
         })
         .catch((error) => {
             console.log(error)

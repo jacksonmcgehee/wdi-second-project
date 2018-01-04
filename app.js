468f10e16925b484d214069ab7b1e29cde963508
jacksonmcgehee <jacksonmcgehee@gmail.com>
2018-01-03 19:46:14 -0500

    Added an image to the homepage

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const index = require('./routes/index.js')
+const users = require('./routes/users.js')
 
 const app = express()
 

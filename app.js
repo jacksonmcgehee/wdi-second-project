dff2c61ca362fea64c2c8d0849cd3b61d794cebf
jacksonmcgehee <jacksonmcgehee@gmail.com>
2018-01-06 20:34:23 -0500

    Tried to add a favicon; not sure how to do it.

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ mongoose.connection.on('error', (error) => {
 })
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
